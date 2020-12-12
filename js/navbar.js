2bddcc5c053689bada88ff057040dc268567647b
nielskrijger <niels@kryger.nl>
2020-12-12 10:49:05 +0100

    fix: add higlight buttons

--- js/navbar.js
+++ js/navbar.js
@@ -1,122 +1,3 @@
-const tagColors = [
-  '#00ff00',
-  '#ff0000',
-  '#021aee',
-  '#20c0FF',
-  '#d602ee',
-  '#ffff03',
-  '#FF3D00',
-  '#40FFFF',
-  '#ff0266',
-  '#90ee02',
-  '#80CBC4',
-  '#BCAAA4',
-  '#ee6002',
-  '#709Ddd',
-  '#FFC107',
-];
-
-const mix = (start, end, percent) => {
-  return start + percent * (end - start);
-};
-
-const mixColors = (color1, color2, percent) => {
-  const red1 = parseInt(color1[1] + color1[2], 16);
-  const green1 = parseInt(color1[3] + color1[4], 16);
-  const blue1 = parseInt(color1[5] + color1[6], 16);
-
-  const red2 = parseInt(color2[1] + color2[2], 16);
-  const green2 = parseInt(color2[3] + color2[4], 16);
-  const blue2 = parseInt(color2[5] + color2[6], 16);
-
-  const red = Math.round(mix(red1, red2, percent));
-  const green = Math.round(mix(green1, green2, percent));
-  const blue = Math.round(mix(blue1, blue2, percent));
-
-  const r = red.toString(16).padStart(2, '0');
-  const g = green.toString(16).padStart(2, '0');
-  const b = blue.toString(16).padStart(2, '0');
-  return '#' + r + g + b;
-};
-
-/**
- * Returns all elements with classname "tag" and groups them in a Map by their common text.
- */
-const findTagGroups = () => {
-  const tags = document.getElementsByClassName('tag');
-
-  const groupedTags = new Map();
-  for (let tag of tags) {
-    const text = tag.innerText;
-    if (!groupedTags.has(text)) {
-      groupedTags.set(text, []);
-    }
-    groupedTags.set(text, [...groupedTags.get(text), tag]);
-  }
-
-  // Order map alphabetically so that duplicate colors are less likely to be adjacent
-  return new Map([...groupedTags.entries()].sort());
-};
-
-const activeTags = [];
-
-const handleMouseOverTag = (tags, color) => () => {
-  for (const tag of tags) {
-    if (!activeTags.includes(tag)) {
-      tag.style.borderColor = mixColors(color, '#000000', 0.4);
-      tag.style.backgroundColor = mixColors(color, '#000000', 0.7);
-      tag.style.boxShadow = `1px 0 10px 1px ${mixColors(color, '#000000', 0.6)}`;
-    }
-  }
-};
-
-const resetTagStyle = (tag) => {
-  tag.style.borderColor = null;
-  tag.style.backgroundColor = null;
-  tag.style.boxShadow = null;
-  tag.style.color = null;
-};
-
-const handleMouseOutTag = (tags) => () => {
-  for (const tag of tags) {
-    if (!activeTags.includes(tag)) {
-      resetTagStyle(tag);
-    }
-  }
-};
-
-const handleClickTag = (tags, color) => () => {
-  for (const tag of tags) {
-    const index = activeTags.indexOf(tag);
-    if (index > -1) {
-      activeTags.splice(index, 1);
-      resetTagStyle(tag);
-    } else {
-      activeTags.push(tag);
-      tag.style.color = 'white';
-      tag.style.borderColor = mixColors(color, '#000000', 0.3);
-      tag.style.backgroundColor = mixColors(color, '#000000', 0.6);
-      tag.style.boxShadow = `1px 0 10px 1px ${mixColors(color, '#000000', 0.4)}`;
-    }
-  }
-};
-
-const initTagHighlighting = () => {
-  const groupedTags = findTagGroups().values();
-  let i = 0;
-  for (const tagGroup of groupedTags) {
-    for (const tag of tagGroup) {
-      tag.addEventListener(
-        'mouseover',
-        handleMouseOverTag(tagGroup, tagColors[i % tagColors.length])
-      );
-      tag.addEventListener('mouseout', handleMouseOutTag(tagGroup));
-      tag.addEventListener('click', handleClickTag(tagGroup, tagColors[i % tagColors.length]));
-    }
-    i++;
-  }
-};
-
 /**
  * Makes the navbar sticky once the browser scrolls below the original navbar position.
  *
@@ -169,7 +50,7 @@ const getNavbarPages = () => {
  * Due to "scroll-behaviour: smooth" scrolling takes a while and other effects
  * need to sync-up with this motion.
  */
-class ScrollingMotion {
+class ScrollMotion {
   constructor() {
     this.scrollingFrom = null;
     this.scrollingTo = null;
@@ -195,7 +76,7 @@ class ScrollingMotion {
   }
 }
 
-const scrollingMotion = new ScrollingMotion();
+const scrollingMotion = new ScrollMotion();
 
 /**
  * Changes the navbar styling when clicking or scrolling through the page.
@@ -326,16 +207,8 @@ const initNavbarHorizontalMotion = () => {
   handleScroll();
 };
 
-const scrollDownArrow = () => {
-  // timeout necessary for FF and Safari to fix issue with innerHeight not set yet
-  setTimeout(() => {
-    window.scrollTo({ top: window.innerHeight, left: 0 });
-  }, 5);
-};
-
-window.onload = () => {
-  initTagHighlighting();
+window.addEventListener('load', () => {
   initNavbarStickiness();
   initNavbarActive();
   initNavbarHorizontalMotion();
-};
+});
---
+++ js/tags.js
@@ -0,0 +1,185 @@
+const tagColors = [
+  '#00ff00',
+  '#ff0000',
+  '#021aee',
+  '#20c0FF',
+  '#d602ee',
+  '#ffff03',
+  '#FF3D00',
+  '#40FFFF',
+  '#ff0266',
+  '#90ee02',
+  '#80CBC4',
+  '#BCAAA4',
+  '#ee6002',
+  '#709Ddd',
+  '#FFC107',
+];
+
+const mix = (start, end, percent) => {
+  return start + percent * (end - start);
+};
+
+const mixColors = (color1, color2, percent) => {
+  const red1 = parseInt(color1[1] + color1[2], 16);
+  const green1 = parseInt(color1[3] + color1[4], 16);
+  const blue1 = parseInt(color1[5] + color1[6], 16);
+
+  const red2 = parseInt(color2[1] + color2[2], 16);
+  const green2 = parseInt(color2[3] + color2[4], 16);
+  const blue2 = parseInt(color2[5] + color2[6], 16);
+
+  const red = Math.round(mix(red1, red2, percent));
+  const green = Math.round(mix(green1, green2, percent));
+  const blue = Math.round(mix(blue1, blue2, percent));
+
+  const r = red.toString(16).padStart(2, '0');
+  const g = green.toString(16).padStart(2, '0');
+  const b = blue.toString(16).padStart(2, '0');
+  return '#' + r + g + b;
+};
+
+/**
+ * Returns all elements with classname "tag" and groups them in a Map by their text.
+ */
+const findTagGroups = () => {
+  const tags = document.getElementsByClassName('tag');
+
+  const tagGroups = new Map();
+  for (let i = 0; i < tags.length; i++) {
+    const text = tags[i].innerText;
+    let group = tagGroups.get(text);
+    if (!group) {
+      group = { tags: [], color: tagColors[i % tagColors.length] };
+    }
+    tagGroups.set(text, { ...group, tags: [...group.tags, tags[i]] });
+  }
+
+  // Order map alphabetically so that duplicate colors are less likely to be adjacent
+  return tagGroups;
+};
+
+const activeTags = new Set();
+
+const handleMouseOverTag = ({ tags, color }) => () => {
+  for (const tag of tags) {
+    if (!activeTags.has(tag)) {
+      tag.style.borderColor = mixColors(color, '#111111', 0.4);
+      tag.style.backgroundColor = mixColors(color, '#111111', 0.7);
+      tag.style.boxShadow = `1px 0 10px 1px ${mixColors(color, '#111111', 0.6)}`;
+    }
+  }
+};
+
+const activateTag = (tag, color) => {
+  activeTags.add(tag);
+  tag.classList.add('tag--active');
+  tag.style.color = 'white';
+  tag.style.borderColor = mixColors(color, '#111111', 0.3);
+  tag.style.backgroundColor = mixColors(color, '#111111', 0.6);
+  tag.style.boxShadow = `1px 0 10px 1px ${mixColors(color, '#111111', 0.4)}`;
+
+  checkTagHighlightButton(tag);
+};
+
+const deactivateTag = (tag) => {
+  activeTags.delete(tag);
+  tag.classList.remove('tag--active');
+  tag.style.color = null;
+  tag.style.borderColor = null;
+  tag.style.backgroundColor = null;
+  tag.style.boxShadow = null;
+
+  checkTagHighlightButton(tag);
+};
+
+const handleMouseOutTag = (tags) => () => {
+  for (const tag of tags) {
+    if (!activeTags.has(tag)) {
+      deactivateTag(tag);
+    }
+  }
+};
+
+const handleClickTag = ({ tags, color }) => () => {
+  for (const tag of tags) {
+    if (activeTags.has(tag)) {
+      deactivateTag(tag);
+    } else {
+      activateTag(tag, color);
+    }
+  }
+};
+
+const initTagHighlighting = () => {
+  const groupedTags = findTagGroups().values();
+  let i = 0;
+  for (const tagGroup of groupedTags) {
+    for (const tag of tagGroup.tags) {
+      tag.addEventListener('mouseover', handleMouseOverTag(tagGroup));
+      tag.addEventListener('mouseout', handleMouseOutTag(tagGroup.tags));
+      tag.addEventListener('click', handleClickTag(tagGroup));
+    }
+    i++;
+  }
+};
+
+const findTagHighlightButtons = () => {
+  const highlightButtons = new Map();
+  const buttons = document.getElementsByClassName('highlight-tags');
+  for (const button of [...buttons]) {
+    const tags = button.getAttribute('data-tags').split(',');
+    highlightButtons.set(button, tags);
+  }
+  return highlightButtons;
+};
+
+const initTagHighlightButtons = () => {
+  const tagGroups = findTagGroups();
+  for (const [button, highlightTags] of findTagHighlightButtons().entries()) {
+    button.addEventListener('click', () => {
+      const isActive = button.classList.contains('highlight-tags--active');
+      highlightTags.forEach((highlightTag) => {
+        const { tags, color } = tagGroups.get(highlightTag);
+        for (const tag of tags) {
+          if (!isActive) {
+            activateTag(tag, color);
+          } else {
+            deactivateTag(tag);
+          }
+        }
+      });
+    });
+  }
+};
+
+/**
+ * Checks if all tags of a highlight button are activated. If so adds the --active
+ * class to the button, or removes it if there's at least one tag not higlighted.
+ */
+const checkTagHighlightButton = (tag) => {
+  // All the lookups every time are a bit inefficient, but saw no performance issues.
+  // Some better state management wouldn't hurt though.
+  const buttons = Array.from(findTagHighlightButtons().entries());
+  const result = buttons.find(([, buttonTags]) => buttonTags.includes(tag.innerText));
+  if (!result) {
+    return; // No highlight button for this tag exists
+  }
+
+  const tagGroups = findTagGroups();
+  const [button, highlightTags] = result;
+  const allTagsActive = highlightTags.every((highlightTag) =>
+    tagGroups.get(highlightTag).tags.every((element) => activeTags.has(element))
+  );
+
+  if (allTagsActive) {
+    button.classList.add('highlight-tags--active');
+  } else {
+    button.classList.remove('highlight-tags--active');
+  }
+};
+
+window.addEventListener('load', () => {
+  initTagHighlighting();
+  initTagHighlightButtons();
+});
